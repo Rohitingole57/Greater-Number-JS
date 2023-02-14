1df2a3df73a01638ca03e06f6fec12701349faaa
Rohit Ingole <ingolerohit2017@gmail.com>
2023-02-14 11:27:41 +0530

    Greter value

---
+++ index.js
@@ -0,0 +1,11 @@
+var a = 10;
+// var c = prompt("Enter Value: ");
+var b=11;
+if ( a>b){
+    console.log("maja maa");
+    console.log(a);
+}
+    else{
+        console.log("kem cho");
+    console.log(b);
+    }
